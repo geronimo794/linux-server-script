ce4344b53f1ea964fb4b4eaa81d28cf24c2d4276
Ach Rozikin <geronimo794@gmail.com>
2023-05-03 09:32:53 +0700

    fix: Fix sshd

--- disable-lang-forwarding-sshd.sh
+++ disable-lang-forwarding-sshd.sh
@@ -1,2 +1,4 @@
 # Comment out the line on /etc/ssh/sshd_config
-AcceptEnv LANG LC_*
+# AcceptEnv LANG LC_*
+sudo sed -i 's/AcceptEnv LANG LC_*/#AcceptEnv LANG LC_*/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
