7db3f16765cfa639dfa2deb0dd87520e741b8cad
Ach Rozikin <geronimo794@gmail.com>
2023-05-07 22:19:57 +0700

    fix: Update caption

--- install-automatic-docker-prune.sh
+++ install-automatic-docker-prune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define and save file
-echo "Add docker prune command to /etc/cron.daily/docker-prune"
+echo "Add docker prune command to /etc/cron.daily/docker-prune ..."
 
 echo '#!/bin/bash
 sudo docker system prune -af --filter "until=$((2*24))h"
@@ -10,8 +10,5 @@ sudo docker builder prune -af --filter "until=24h"
 
 sudo chmod +x /etc/cron.daily/docker-prune
 
-echo "File successfully added"
+echo "Add docker prune command to /etc/cron.daily/docker-prune finished!"
 echo "You try cron dail with: 'run-parts /etc/cron.daily'"
-
-
-
